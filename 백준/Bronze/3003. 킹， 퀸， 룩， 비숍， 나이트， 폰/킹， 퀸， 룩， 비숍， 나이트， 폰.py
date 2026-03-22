e8530bb5edd7685d07501a0b36672ace6dbe40e5
홍준 <96008006+flos216@users.noreply.github.com>
2026-03-22 17:24:44 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,43 +1,6 @@
-a, b, c, d, e, f = map(int, input().split())
+chess = [1, 1, 2, 2, 2, 8]
 
-if a == 1:
-    print(0, end=" ")
-elif a < 1:
-    print(1-a, end=" ")
-elif a > 1:
-    print(1-a, end=" ")
-    
-if b == 1:
-    print(0, end=" ")
-elif b < 1:
-    print(1-b, end=" ")
-elif b > 1:
-    print(1-b, end=" ")
-    
-if c == 2:
-    print(0, end=" ")
-elif c < 2:
-    print(2-c, end=" ")
-elif c > 2:
-    print(2-c, end=" ")
-    
-if d == 2:
-    print(0, end=" ")
-elif d < 2:
-    print(2-d, end=" ")
-elif d > 2:
-    print(2-d, end=" ")
-    
-if e == 2:
-    print(0, end=" ")
-elif e < 2:
-    print(2-e, end=" ")
-elif e > 2:
-    print(2-e, end=" ")
-    
-if f == 8:
-    print(0)
-elif f < 8:
-    print(8-f)
-elif f > 8:
-    print(8-f)
+a = list(map(int, input().split()))
+
+for i in range(len(chess)):
+    print(chess[i] - a[i], end=' ')
