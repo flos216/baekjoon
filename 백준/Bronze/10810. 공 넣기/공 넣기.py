f06a85e182a3d85bb6db272f8d81b5641386bfd3
홍준 <96008006+flos216@users.noreply.github.com>
2026-03-22 17:26:55 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+a = [0]*n
+
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for x in range(i-1, j):
+        a[x] = k
+
+for x in range(n):
+    print(a[x], end=" ")
+        
+    
